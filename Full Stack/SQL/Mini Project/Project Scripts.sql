3f490f591c6448ea82ebd9905fed21e104006a1c
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-02-29 16:52:09 +0530

    files edited

--- Full Stack/SQL/Mini Project/Project Scripts.sql
+++ Full Stack/SQL/Mini Project/Project Scripts.sql
@@ -11,6 +11,7 @@ alter table emp_details add constraint primary key(emp_id);
 select * from sal_details;
 describe sal_details;
 alter table sal_details add constraint primary key(salary_id);
+alter table sal_details rename column `branch id` to branch_id;
 alter table sal_details modify salary_date date;
 
 select * from desig_details;
@@ -25,3 +26,35 @@ alter table dept_details add constraint primary key(dep_no);
 
 create database dummy01;
 drop database dummy01;
+
+show tables;
+
+insert into emp_details values (17034, 'Vijay', 3003, 50, '2022-08-31');
+
+create table dummy_table (dummy_id int, dummy_name varchar(40), age int, marks int);
+
+drop table dummy_table;
+
+alter table emp_details add column new_column varchar(20);
+
+alter table emp_details modify new_column int;
+
+alter table emp_details drop column new_column;
+
+alter table emp_details rename employee_details;
+alter table employee_details rename emp_details;
+
+update emp_details set emp_name = 'Vijay Adithya' where emp_id = 17034;
+delete from emp_details where emp_id = 17034;
+
+select * from emp_details WHERE dep_no = 70;
+
+select * from sal_details where (branch_id = 241 or branch_id = 242)
+and amount >= 15000;
+
+select * from sal_details where branch_id in (243, 244);
+
+select * from sal_details where branch_id not in (241, 242);
+
+select * from sal_details where amount >= 25000;
+select * from sal_details where amount < 20000;
