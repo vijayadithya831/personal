5c93ea2e1a187e9b23acd1bf2a278e67d29458be
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-03-26 09:41:45 +0530

    files edited

--- Full Stack/Python/BMI.py
+++ Full Stack/Python/BMI.py
@@ -6,5 +6,5 @@
 name = input("Enter your name: ")
 weight = float(input("Enter your weight in KG: "))
 height = float(input("Enter your height in meters: "))
-bmi = weight/(height**height)
-print(f"Hi {name}, Your weight is {weight} kgs, Your height is {height} m. So, Your BMI is {bmi}")
+bmi = round(weight/(height**2),2)
+print(f"Hi {name}, Your weight is {weight} kg, Your height is {height} m. So, Your BMI is {bmi}")
