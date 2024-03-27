57e929f934ae9c88dcbafbdcbae3d8dcb8352067
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-03-27 13:11:41 +0530

    files edited

--- Full Stack/Python/num_compare.py
+++ Full Stack/Python/num_compare.py
@@ -1,6 +1,6 @@
-a = int(input("Enter the value of num1: "))
-b = int(input("Enter the value of num2: "))
-c = int(input("Enter the value of num3: "))
+num1 = int(input("Enter the value of num1: "))
+num2 = int(input("Enter the value of num2: "))
+num3 = int(input("Enter the value of num3: "))
 
 if num1 > num2 and num1 > num3:
     print(f"{num1} is greater than {num2} and {num3}")
