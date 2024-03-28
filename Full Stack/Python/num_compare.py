866c6ff768f7814848805bd04718f6d454c61e2e
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-03-28 09:13:37 +0530

    files edited

--- Full Stack/Python/num_compare.py
+++ Full Stack/Python/num_compare.py
@@ -10,20 +10,3 @@ else:
     print(f"{num3} is greater than {num1} and {num2}")
 
 # complete this code using nested if and optimize it to the maximum
-# if a == b == c:
-#     print("a,b,c are equal")
-# if a == b and a != c:
-#     if c > a:
-#         print("c is larger, a and b are equal")
-#     else:
-#         print("c is smaller, a and b are equal")
-# if a == c and a != b:
-#     if b > a:
-#         print ("b is larger, a and c are equal")
-#     else:
-#         print("b is smaller, a and c are equal")
-# if b == c and b != a:
-#     if a > b:
-#         print("a is larger, b and c are equal")
-#     else:
-#         print("a is smaller, b and c are equal")
