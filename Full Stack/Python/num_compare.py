7472ef80e170b790aa48a0235321c8632c2777fa
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-03-28 09:35:53 +0530

    files added and edited

--- Full Stack/Python/num_compare.py
+++ Full Stack/Python/num_compare.py
@@ -2,11 +2,20 @@ num1 = int(input("Enter the value of num1: "))
 num2 = int(input("Enter the value of num2: "))
 num3 = int(input("Enter the value of num3: "))
 
-if num1 > num2 and num1 > num3:
-    print(f"{num1} is greater than {num2} and {num3}")
-elif num2 > num1 and num2 > num3:
-    print(f"{num2} is greater than {num1} and {num3}")
-else:
-    print(f"{num3} is greater than {num1} and {num2}")
+# if num1 > num2 and num1 > num3:
+#     print(f"{num1} is greater than {num2} and {num3}")
+# elif num2 > num1 and num2 > num3:
+#     print(f"{num2} is greater than {num1} and {num3}")
+# else:
+#     print(f"{num3} is greater than {num1} and {num2}")
 
 # complete this code using nested if and optimize it to the maximum
+if(num1 > num2):
+    if(num1 > num3):
+        print("num1 is greater")
+    else:
+        print("num3 is greater")
+elif(num2 > num3):
+    print("num2 is greater")
+else:
+    print("num3 is greater")
