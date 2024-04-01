93e410e7d2b93d06c8f58ba95059f90249a1fb66
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-04-01 11:08:05 +0530

    files edited

--- Full Stack/Python/Tasks/circle_area.py
+++ Full Stack/Python/Tasks/circle_area.py
@@ -2,5 +2,11 @@ print("Time to solve some Mensuration Problems! We shall now find the area of a 
 print("Note: The value of PI is taken as 3.14 by default")
 r = int(input("Enter the Radius of the circle: "))
 PI = 3.14
-area = round((PI * r * r),3)
-print(f"The Area of the Circle is {area} square units.")
+def circleArea(r):
+    area = PI * r * r
+    return round(area,2)
+def circleCircumference(r):
+    circumference = 2 * PI * r
+    return round(circumference,2)
+print(f"The Area of the Circle of radius {r} is {circleArea(r)} square units")
+print(f"The Circumference of the Circle of radius {r} is {circleCircumference(r)} units")
--- Full Stack/Python/Tasks/hcf_lcm.py
+++ Full Stack/Python/Tasks/hcf_lcm.py
@@ -1,20 +1,25 @@
 print("We are now going to find out the Highest Common Factor and Least Commom Multiple for a given pair of numbers")
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
-a = num1
-b = num2
-if not(a>b):
-    c = a
-    a = b
-    b = c
-q = 0
-r = 1
-while r!=0:
-    q = a//b
-    r = a%b
-    a = b
-    b = r
-hcf = a
-lcm = int((num1 * num2)/hcf)
-print(f"The Highest Common Factor of {num1} and {num2} is {hcf}")
-print(f"The Least Common Multiple of {num1} and {num2} is {lcm}")
+def calcHcf(a,b):
+    global hcf
+    x = a
+    y = b
+    if not(x>y):
+        c = x
+        x = y
+        y = c
+    q = 0
+    r = 1
+    while r!=0:
+        q = x//y
+        r = x%y
+        x = y
+        y = r
+    hcf = x
+    return hcf
+def calcLcm(a,b):
+    lcm = int((a * b)/hcf)
+    return lcm
+print(f"The Highest Common Factor of {num1} and {num2} is {calcHcf(num1,num2)}")
+print(f"The Least Common Multiple of {num1} and {num2} is {calcLcm(num1,num2)}")
