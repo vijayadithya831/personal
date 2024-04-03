fa7e787930c09d379cf7c4acb86e0607968c30d5
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-04-03 10:24:01 +0530

    files edited

--- Full Stack/Python/Tasks/Simple Calculator/functions.py
+++ Full Stack/Python/Tasks/Simple Calculator/functions.py
@@ -1,14 +1,21 @@
 def add(a,b):
+    "This functions takes 2 inputs and returns their sum"
     return a+b
 def sub(a,b):
+    "This function takes 2 inputs and returns their difference"
     return a-b
 def mul(a,b):
+    "This function takes 2 inputs and returns their product"
     return a*b
 def div(a,b):
+    "This function takes 2 inputs and returns the decimal value of their fraction"
     return a/b
 def mod(a,b):
+    "This function takes 2 inputs and returns the reminder when divided"
     return a%b
 def flr(a,b):
+    "This function takes 2 inputs and returns the quotient when divided"
     return a//b
 def exp(a,b):
+    "This function takes 2 inputs and returns the exponent value"
     return a**b
