cb068e372e34108e32340dc7d93580f7df840fda
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-04-18 16:50:11 +0530

    files edited

--- Full Stack/Django/myweb/myapp/views.py
+++ Full Stack/Django/myweb/myapp/views.py
@@ -1,9 +1,20 @@
+from django.views.decorators.csrf import csrf_exempt
 from django.shortcuts import render
+from myapp.models import Students
 
 # Create your views here.
+@csrf_exempt
 def home(request):
     return render(request, "home.html")
 
 def register(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        phone = request.POST.get("phone")
+
+        student = Students(Name=name,Email=email,Phone=phone)
+        student.save()
+
     return render(request, "register.html")
 
