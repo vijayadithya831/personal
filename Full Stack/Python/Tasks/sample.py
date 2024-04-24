563bf9d0670a8d8193b2b719167155d8f315c543
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-04-24 18:33:27 +0530

    files edited

--- Full Stack/Python/Tasks/sample.py
+++ Full Stack/Python/Tasks/sample.py
@@ -1,4 +1,18 @@
-a = int(input("Enter value for a "))
-b = int(input("Enter value for b "))
-c = a + b
-print("the sum of", a,"33and", b,"is", c)
+n = int(input("enter a number : "))
+# t = n
+if n %2 != 0:
+    for i in range(n):
+        for j in range(n):
+            if i == j and i < (n+1)//2:
+                print(n-i, end = '')
+            if i+j == n-1 and i < (n+1)//2 - 1:
+                print(i+1, end = '')
+            if i+j == n-1 and i>= (n+1)//2:
+                print(j+1, end = '')
+            if i == j and i>= (n+1)//2:
+                print(i+1, end = '')
+            else:
+                print(' ', end = '')
+        print()
+else:
+    print("Enter an odd number")
