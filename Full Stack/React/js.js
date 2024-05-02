4541f15756664dbfd90101f900a0c33fd079c41b
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-05-02 09:50:05 +0530

    files updated

--- Full Stack/React/js.js
+++ Full Stack/React/js.js
@@ -1,15 +1,23 @@
-let a = 1;
+// let a = 1;
 let b = 2;
 let c = 3;
 let d = 4;
 let e = 5;
 
 function sum(...numbers) {
-    let total = 1;
-    for( let number of numbers){
-        total *= number;
-    }
+    // let total = 1;
+    let total = numbers.reduce((sum, number) => {
+        return sum + number;
+     } ,0);
+//     for( let number of numbers){
+//         total *= number;
+//     }
     return total;
 }
 
-console.log(sum(a,b,c,d,e));
+console.log(sum(a,b,c,d));
+
+
+function displayConsole(output) {
+    console.log(output);
+}
