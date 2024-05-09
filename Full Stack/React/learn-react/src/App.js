d0d7848112f474ec4fead3eada0100ffc7ebf02a
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-05-09 09:25:07 +0530

    files edited

--- Full Stack/React/learn-react/src/App.js
+++ Full Stack/React/learn-react/src/App.js
@@ -3,21 +3,9 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>A
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world</h1>
+      <p>This is a sample text to show that react app works just fine</p>
     </div>
   );
 }
