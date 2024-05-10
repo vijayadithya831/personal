2505442c01e102d1ce51e8bc6414ae1752f5833d
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-05-10 09:26:10 +0530

    files updated

--- Full Stack/React/learn-react/src/App.js
+++ Full Stack/React/learn-react/src/App.js
@@ -4,7 +4,6 @@ import Greetings from './Components/WelcomeFuncCompPractice'
 import WelcomeStudents from './Components/WelcomeClassCompPractice';
 
 function App() {
-  // return <h1>Welcome</h1>;
   return (
     <div className='App'>
       <Greetings/>
