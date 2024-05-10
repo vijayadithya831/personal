ab0e10a0cd4bb6bf85589c96452ceaee44983ccb
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-05-10 09:13:56 +0530

    files updated

--- Full Stack/React/learn-react/src/App.js
+++ Full Stack/React/learn-react/src/App.js
@@ -1,12 +1,13 @@
 // import logo from './logo.svg';
 import './App.css';
-
+import Greetings from './Components/WelcomeFuncCompPractice'
 import WelcomeStudents from './Components/WelcomeClassCompPractice';
 
 function App() {
   // return <h1>Welcome</h1>;
   return (
     <div className='App'>
+      <Greetings/>
       <WelcomeStudents/>
       <WelcomeStudents/>
       <WelcomeStudents/>
---
+++ Full Stack/React/learn-react/src/Components/WelcomeFuncCompPractice.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Greetings() {
+    return (
+        <div>
+            <h1>Welcome To The JavaScript Course</h1>
+        </div>
+    );
+}
+
+export default Greetings;
