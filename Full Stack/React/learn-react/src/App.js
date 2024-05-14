15794ed591680da6ed1b00c80c2b4305053d62f6
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-05-14 18:59:43 +0530

    files updated

--- Full Stack/React/learn-react/src/App.js
+++ Full Stack/React/learn-react/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import {Welcome, Button, Form} from './Components/WelcomeFuncCompPractice'
 import WelcomeStudents from './Components/WelcomeClassCompPractice';
@@ -8,19 +8,19 @@ import {CounterClassState, ChangeText} from './Components/CounerClassState';
 function App() {
   return (
     <div className='App'>
-        {/* <Greetings/>
-        <WelcomeStudents/>
+        {/* <Welcome name = "Vijay" sportsName = "Cricket" age = "21" gender = "Male"/>
+        <WelcomeStudents name = "Vijay" sportsName = "Cricket"/>
         <h1>These two messages are rendered from function and class component respectively!</h1> */}
         {/* <Welcome name = "Ganesh" sportsName = "Cricket"/>
         <Welcome name = "Aslam" sportsName = "Football"/>
         <Welcome name = "John" sportsName = "Hockey"/>
         <WelcomeStudents name = "Ganesh" sportsName = "Cricket"/>
         <WelcomeStudents name = "Aslam" sportsName = "Football"/>
-        <WelcomeStudents name = "John" sportsName = "Hockey"/> */}
-        {/* <Welcome name = "Ganesh" sportsName = "Cricket">
+        <WelcomeStudents name = "John" sportsName = "Hockey"/>
+        <Welcome name = "Ganesh" sportsName = "Cricket">
             <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Temporibus dicta optio omnis voluptatum consectetur cumque molestias aliquid quae sit similique sunt voluptatem, magnam ex ipsa sed consequuntur. Fugiat distinctio eveniet beatae libero minima veritatis doloribus dolore, ipsam exercitationem hic, quod tenetur natus, odio porro dicta pariatur? Reiciendis quo laborum eum.</p>
-        </Welcome>
-        <Welcome name = "Aslam" sportsName = "Football">
+        </Welcome> */}
+        {/* <Welcome name = "Aslam" sportsName = "Football">
             <button>Click Me</button>
         </Welcome>
         <Welcome name = "John" sportsName = "Hockey">
