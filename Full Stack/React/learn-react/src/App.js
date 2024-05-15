58045e25e8a3f5ca96bf59f6fe67f73416399ec4
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-05-15 10:19:37 +0530

    files updated

--- Full Stack/React/learn-react/src/App.js
+++ Full Stack/React/learn-react/src/App.js
@@ -4,7 +4,7 @@ import {Welcome1, Welcome2, Buttons, Forms} from './Components/WelcomeFuncCompPr
 import WelcomeStudents from './Components/WelcomeClassCompPractice';
 import Information from './Components/information';
 import {CounterClassState, ChangeText} from './Components/CounerClassState';
-import StudentWelcomeConditionalIfElse from './Components/StudentWelcomeConditionalIfElse';
+import { StudentWelcomeConditionalIfElse1, StudentWelcomeConditionalIfElse2 } from './Components/StudentWelcomeConditionalIfElse';
 
 function App() {
   return (
@@ -42,7 +42,8 @@ function App() {
         {/* <Information/> */}
         {/* <CounterClassState/> */}
         {/* <ChangeText/> */}
-        <StudentWelcomeConditionalIfElse name="Vijay"/>
+        <StudentWelcomeConditionalIfElse1 name="Vijay"/>
+        <StudentWelcomeConditionalIfElse2 name="Vijay"/>
         
         
     </div>
--- Full Stack/React/learn-react/src/Components/StudentWelcomeConditionalIfElse.js
+++ Full Stack/React/learn-react/src/Components/StudentWelcomeConditionalIfElse.js
@@ -1,30 +1,50 @@
 import React from "react";
 
-class StudentWelcomeConditionalIfElse extends React.Component {
+class StudentWelcomeConditionalIfElse1 extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            studentLoggedIn: false
+        };
+        var info = null;
+    }
+    render() {
+        if(this.state.studentLoggedIn) {
+            this.info = <div><h1>Welcome {this.props.name}</h1></div>
+            // return(
+            //     <div>
+            //         <h1>Welcome {this.props.name}</h1>
+            //     </div>
+            // );
+        }
+
+        else {
+            this.info = <div><h1>Welcome Students</h1></div>
+            // return(
+            //     <div>
+            //         <h1>Welcome Students</h1>
+            //     </div>
+            // );
+        }
+        return <div>{this.info}</div>
+    }
+}
+
+class StudentWelcomeConditionalIfElse2 extends React.Component {
 
     constructor(props) {
         super(props)
         this.state = {
             studentLoggedIn: true
         };
+        var info = null;
     }
     render() {
-        if(this.state.studentLoggedIn) {
-            return(
-                <div>
-                    <h1>Welcome {this.props.name}</h1>
-                </div>
-            );
-        }
-
-        else {
-            return(
-                <div>
-                    <h1>Welcome Students</h1>
-                </div>
-            );
-        }
+        return(
+            this.state.studentLoggedIn ? <div><h1>Welcome {this.props.name}</h1></div> : <div><h1>Welcome Students</h1></div>
+        );
     }
 }
 
-export default StudentWelcomeConditionalIfElse;
+export { StudentWelcomeConditionalIfElse1, StudentWelcomeConditionalIfElse2 };
