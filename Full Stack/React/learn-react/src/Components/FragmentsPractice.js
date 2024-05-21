265db776a2b2b16f3172a4d470b1cb5d6a2fe577
Vijay Adithya <rs.vijayadithya831@gmail.com>
2024-05-21 09:27:55 +0530

    file created

---
+++ Full Stack/React/learn-react/src/Components/FragmentsPractice.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FragmentsPractice() {
+  return (
+    <div>
+        <h1>Studying about Fragments in React</h1>
+    </div>
+  )
+}
+
+export default FragmentsPractice;
